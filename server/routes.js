c43456eb318ee2f4a635be84eecf73b8b81092bc
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2022-10-01 20:41:33 +0200

    Add basic routing
    Closes #27

---
+++ server/routes.js
@@ -0,0 +1,8 @@
+export { routes as default };
+
+const routes = {
+  '/': './public/ui/index.html',
+  '/recipes': './data/recipes/recipe-1.txt',
+  '/ingredients': './data/ingredients/ingredients.json',
+  '/contact': './public/pages/contact.html'
+};
