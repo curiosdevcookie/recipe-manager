6cd5153a04b1f9105b3d30fce5dfbf46ac1b756b
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2022-10-03 18:16:37 +0200

    Add static functionality to serve files and basic routing

--- server.js
+++ server.js
@@ -11,32 +11,50 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 const server = http.createServer((req, res) => {
   let parsedURL = parse(req.url, true);
   let path = parsedURL.path.replace(/^\/+|\/+$/g, '');
+
+  console.log(`Request received on path: ${routes}`);
+  console.log(`Request received on path: ${path}`);
 
   if (path === '') {
     path = 'index.html'
   }
 
-  // console.log(`Request received on path: ${routes}`);
+  const file = __dirname + '/public/' + path;
+  const { pathname } = parse(req.url);
+  const route = routes[pathname];
 
-  let file = __dirname + '/public/' + path;
-
-  readFile(file, (err, content) => {
-    if (err) {
-      res.writeHead(404);
-      res.end('Not Found');
-    } else {
-      res.setHeader('X-Content-Type-Options', 'nosniff');
-      res.writeHead(200, { 'Content-Type': lookup(path) });
-      res.end(content);
-    }
-  });
+  if (route) {
+    readFile(route, (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error');
+      } else {
+        res.writeHead(200);
+        res.end(data);
+      }
+    });
+  } else {
+    readFile(file, (err, content) => {
+      if (err) {
+        res.writeHead(404);
+        res.end('Not Found');
+      } else {
+        res.setHeader('X-Content-Type-Options', 'nosniff');
+        res.writeHead(200, { 'Content-Type': lookup(path) });
+        res.end(content);
+      }
+    });
+  }
 });
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
 
-console.log(__dirname);
+// console.log(__dirname/*, __filename*/);
+// console.log(http.METHODS);
+// console.log(http.StatusCodes);
